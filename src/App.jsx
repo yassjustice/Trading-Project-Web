f8e2bc7bfc1c998d8cadd548e2749b6ce87879c6
Yassir hakimi <yassirhakimi60@gmail.com>
2024-04-01 22:10:29 +0000

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AdminRoutes, { PublicRoutes } from "./utils/AdminRoutes";
+import { Login } from "./pages/auth/Login";
+import MainLayout from "../src/layouts/MainLayout";
+import DashboardProvider from "./context/dashboardContext";
+import Dashboard from "./pages/dashboard/Dashboard";
+import { Register } from "./pages/auth/Register";
+import Profile from "./pages/profile/profile";
+import StockExchange from "./pages/stockxchange/stockExchange";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route element={<PublicRoutes />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Route>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                    <Route element={<AdminRoutes />}>
+                        <Route path="/dashboard" element={<MainLayout />}>
+                            <Route
+                                index
+                                element={
+                                    <DashboardProvider>
+                                        <Dashboard />
+                                    </DashboardProvider>
+                                }
+                            />
+                            <Route path="profile" element={<Profile/>} />
+                            <Route path="stockexchange" element={<StockExchange/>} />
+                        </Route>
+                        {/* <Route path="*" element={<NotFound />} /> */}
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
-export default App
+export default App;
